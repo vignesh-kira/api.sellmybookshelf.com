f0476600bc2e965749354fd449584af51f896f9b
Vigneshwaran Veeran <vigneshwaran.veeran@appdirect.com>
2019-03-19 23:56:01 -0400

    port updated

--- app.js
+++ app.js
@@ -44,6 +44,6 @@ app.use('/classes', require('./controller/Classes'));
 // Sections routes
 app.use('/sections', require('./controller/Sections'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
