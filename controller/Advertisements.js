67a30fc26e7d5c21760f2f3210c484651d8b9153
Vigneshwaran Veeran <vigneshwaran.veeran@appdirect.com>
2019-03-31 22:39:58 -0400

    Advertisement Update works

--- controller/Advertisements.js
+++ controller/Advertisements.js
@@ -4,9 +4,9 @@ const Advertisement = require('../models/Advertisement');
 
 // Get Advertisements list
 router.get('/', (req, res) =>
-    Advertisement.findAll()
-        .then(advertisements => res.send(advertisements))
-        .catch(err => console.log(err)));
+	Advertisement.findAll()
+		.then(advertisements => res.send(advertisements))
+		.catch(err => console.log(err)));
 
 // Create
 router.post('/create', (req, res) => {
@@ -28,6 +28,31 @@ router.post('/create', (req, res) => {
 	Advertisement.create(advertisement)
 		.then(advertisement => res.status(200).json(advertisement))
 		.catch(error => res.sendStatus(409).send(error));
+});
+
+// Create
+router.put('/update/:id', (req, res) => {
+	let { title, description, studentClass, book_title, book_author, condition_text, condition_rating, book_seller_price, book_final_price, subject_id } = req.body;
+
+	let advertisement = Object.assign({
+		title,
+		description,
+		book_title,
+		book_author,
+		condition_text,
+		condition_rating,
+		book_seller_price,
+		book_final_price,
+		class_id: studentClass,
+		subject_id,
+	});
+
+	Advertisement.update(advertisement, {
+		returning: true,
+		where: { id: req.params.id }
+	})
+		.then(() => res.status(200).json(req.body))
+		.catch(error => res.sendStatus(400).send(error));
 });
 
 module.exports = router;
