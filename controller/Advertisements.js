d90935161de20c084a116f5774590a45d300258a
Vigneshwaran Veeran <vigneshwaran.veeran@appdirect.com>
2019-04-01 23:18:25 -0400

    Post Create should be before post find. one advertisement with user id

--- controller/Advertisements.js
+++ controller/Advertisements.js
@@ -23,21 +23,6 @@ router.get('/view/:id', (req, res) => {
 		.catch( error => res.sendStatus(500).send(error))
 });
 
-// Get (ONLY AUTHORIZED USER)
-router.post('/:id', (req, res) => {
-	Advertisement.findOne({
-		where: {id:req.params.id, user_id: req.body.user_id }
-	})
-		.then(advertisement => {
-			if(advertisement == null){
-				res.sendStatus(404);
-			}else{
-				res.status(200).json(advertisement)
-			}
-		})
-		.catch( error => res.sendStatus(500).send(error))
-});
-
 // Create
 router.post('/create', (req, res) => {
 	let { title, description, studentClass, book_title, book_author, condition_text, condition_rating, book_seller_price, book_final_price, user_id, subject_id } = req.body;
@@ -60,7 +45,22 @@ router.post('/create', (req, res) => {
 		.catch(error => res.sendStatus(409).send(error));
 });
 
-// Create
+// Get (ONLY AUTHORIZED USER)
+router.post('/:id', (req, res) => {
+	Advertisement.findOne({
+		where: {id:req.params.id, user_id: req.body.user_id }
+	})
+		.then(advertisement => {
+			if(advertisement == null){
+				res.sendStatus(404);
+			}else{
+				res.status(200).json(advertisement)
+			}
+		})
+		.catch( error => res.sendStatus(500).send(error))
+});
+
+// Update
 router.put('/update/:id', (req, res) => {
 	let { title, description, studentClass, book_title, book_author, condition_text, condition_rating, book_seller_price, book_final_price, subject_id } = req.body;
 
